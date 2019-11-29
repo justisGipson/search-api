01c9127eb4c6ee122afcaca55d69d891c4a511a6
Justis Gipson <justis710@gmail.com>
2019-11-29 12:21:45 -0500

    api-docs finished

--- dist/server.js
+++ dist/server.js
@@ -1,4 +1,27 @@
 "use strict";
-const sum = (a, b) => a + b;
-setInterval(() => console.log(sum(2, 6)), 1000);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const express_1 = __importDefault(require("express"));
+const utils_1 = require("./src/utils");
+const service_1 = __importDefault(require("./src/service"));
+const middleware_1 = __importDefault(require("./src/middleware"));
+const errorHandlers_1 = __importDefault(require("./src/middleware/errorHandlers"));
+process.on('uncaughtException', e => {
+    console.log(e);
+    process.exit(1);
+});
+process.on('unhandledRejection', e => {
+    console.log(e);
+    process.exit(1);
+});
+const router = express_1.default();
+utils_1.applyMiddleware(middleware_1.default, router);
+utils_1.applyRoutes(service_1.default, router);
+utils_1.applyMiddleware(errorHandlers_1.default, router);
+const { PORT = 3000 } = process.env;
+const server = http_1.default.createServer(router);
+server.listen(PORT, () => console.log(`Server is running http://localhost:${PORT}...`));
 //# sourceMappingURL=server.js.map
